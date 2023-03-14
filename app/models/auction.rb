1a3f4c5aaa7d687346ef75414c4fdb11091cc63f
Yan Tselesh <yan.tselesh@radgost.com>
2023-03-14 15:46:56 +0100

    add validation to auction

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -2,7 +2,21 @@ class Auction < ApplicationRecord
   belongs_to :user
   has_many :bids, dependent: :destroy
 
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :starting_price, presence: true, numericality: { greater_than: 0 }
+  validates :end_date, presence: true
+  validate :validate_end_date
+
   def current_price
     bids.present? ? bids.order(price: :desc).first.price : starting_price
+  end
+
+  private
+
+  def validate_end_date
+    if end_date <= Time.zone.now
+      errors.add(end_date: "must be greater than now")
+    end
   end
 end
