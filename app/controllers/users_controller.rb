fc46c84ad6bf35a74052cd7a9b8f9fa7871b2424
Yan Tselesh <yan.tselesh@radgost.com>
2023-03-14 12:56:50 +0100

    add user and auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: [:create]
+  before_action :set_user, only: [:show, :destroy]
+
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
+  # GET /users/:id
+  def show
+    render json: @user, status: :ok
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+
+  end
+
+  # PUT /users/:id
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users
+  def destroy
+    if @user.destroy
+      render json: { message: "user #{@user.username} was deleted!" }, status: :ok
+    else
+      render json: { message: "user #{@user.username} was not deleted!" }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
