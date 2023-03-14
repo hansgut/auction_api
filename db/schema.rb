9e47023d68eed21ed242e0a49524c2083af378f5
Yan Tselesh <yan.tselesh@radgost.com>
2023-03-14 13:35:27 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_105827) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_122307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "auctions", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "end_date"
+    t.float "starting_price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_auctions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_105827) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "auctions", "users"
 end
